0c5b6c36d168b443748d41041932493d89909f3e
mayomayo25 <10bvb25hal45016@gmail.com>
2017-04-25 15:58:30 +0900

    [update] 「あかさたなぞるくん」部分のデザイン・リンクを修正
    ・PDFを埋め込み
    ・リンクを押すとオーバレイ表示するように変更

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(function (){
   // scrollでコード表示
   var code = $("code#code");
   var text = hljs.highlightAuto(code.text()).value;
-  var cursor = "<span id=\"cursor\" />";
+  var cursor = "<span class=\"cursor blinking\" id=\"cursor\">|</span>";
   code.html(cursor);
 
 
@@ -18,6 +18,14 @@ $(function (){
     console.log(scroll);
     if (scroll < 6000) {
       code.html(text.substring(0, Math.round(scroll/5)) + cursor);
+    }
+
+    if(scroll > 500) {
+      $('.scroll_word').removeClass('d_block');
+      $('.scroll_word').addClass('d_none');
+    }else{
+      $('.scroll_word').removeClass('d_none')
+      $('.scroll_word').addClass('d_block');
     }
   });
 
@@ -48,7 +56,57 @@ $(function (){
     set("works",21500);
   });
 
-  $(".contact_link").bind("click", function(){
+  /*$(".contact_link").bind("click", function(){
     set("contact",27000);
+  });*/
+
+
+  //  過去の作品部分
+  $('#works .link').on('click', function(){
+
+    $.when(
+      $('.sec04').find('.overlay').addClass('a_overlay')
+    ).done(function(){
+      setTimeout(function(){
+        $('.sec04').find('.overlay').children('.overlay__content').fadeIn(1000);
+        $('.sec04').find('.overlay').children('.overlay__close').fadeIn(1000);
+      },600);
+
+      no_scroll();
+    });
   });
+
+  //  オーバレイ表示を閉じる
+  $('.overlay__close').on('click', function(){
+    $.when(
+      $('.sec04').find('.overlay').removeClass('a_overlay'),
+      $('.sec04').find('.overlay').addClass('b_overlay')
+    ).done(function(){
+      $('.sec04').find('.overlay').children('.overlay__content').fadeOut(500);
+      $('.sec04').find('.overlay').children('.overlay__close').fadeOut(500);
+      setTimeout(function(){
+        $('.sec04').find('.overlay').removeClass('b_overlay');
+      },800);
+    });
+    return_scroll();
+  });
+
+
+  //スクロール禁止用関数
+  function no_scroll(){
+    //PC用
+    var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
+    $(document).on(scroll_event,function(e){e.preventDefault();});
+    //SP用
+    $(document).on('touchmove.noScroll', function(e) {e.preventDefault();});
+  }
+
+  //スクロール復活用関数
+  function return_scroll(){
+    //PC用
+    var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
+    $(document).off(scroll_event);
+    //SP用
+    $(document).off('.noScroll');
+  }
 });
